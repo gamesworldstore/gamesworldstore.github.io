2b63bf8dc6e98cc3c3ae00717eaf6b84ea389657
Richard Wesley <wesleyrichard30@gmail.com>
2022-08-22 09:55:26 -0300

    alterações e edições

--- js/script.js
+++ js/script.js
@@ -1,46 +1,55 @@
 const div = document.querySelector(".games");
+div.style.minWidth = "100vmin";
+div.style.maxWidth = "50vmax";
 
-class Jogos {
+const jogos = [];
 
-    constructor(){
-        this.jogos = [
-            {"titulo": "Forza Horizon 4","imagem": "/img/forza4.jpg"},
-            {"titulo": "God of War The Game","imagem": "/img/gdwar.jpg"},
-            {"titulo": "Grand Theft Auto 5","imagem": "/img/gta5.jpg"},
-            {"titulo": "Naruto Shippuden Ninja Storm 4", "imagem": "/img/nstorm4.jpg"},
-            {"titulo": "OP Pirate Warriors 4", "imagem": "/img/onep4.jpg"},
-            {"titulo": "Resident Evil 7: Biohazard", "imagem": "/img/resident7.jpg" },
-            {"titulo": "Resident Evil Village", "imagem": "/img/resident8.jpg"},
-            {"titulo": "The Witcher 3 Wild Hunter", "imagem": "/img/witcher3.jpg"}
-        ];
+const montarObjeto = () => {
+    const jogo = document.querySelectorAll("h4.jogo");
+    const imagens = document.querySelectorAll("#gamesIMG");
+    for (let i = 0; i < jogo.length; i++) {
+
+        let object = {};
+        let titulo = jogo[i].textContent;
+        let imagem = imagens[i].src;
+        object.titulo = titulo;
+        object.imagem = imagem;
+        /*const img = document.createElement('img');
+        div.appendChild(img);
+        img = imagem;*/
+        jogos.push(object);
     }
-    /* Lupa de Busca*/
-    mostrar(titulo, imagem){
-        const ul = document.createElement('ul');
-        const li =document.createElement('li');
-        const ptitle = document.createElement('h4');
-        const img = document.createElement('img');
-        div.appendChild(ul);
-        ul.appendChild(li);
-        li.appendChild(img);
-        li.appendChild(ptitle);
-        img.src = imagem;
-        ptitle.textContent = titulo;
-    }
+};
+const mostrar = (titulo, imagem) => {
+    const li =document.createElement('li');
+    const ul = document.createElement('ul').appendChild(li);
 
-    search(){
-        const input = document.querySelector("#searchbar").value;
+    const ptitle = document.createElement('h4');
+    const img = document.createElement('img');
+
+    div.appendChild(ul);
+    li.appendChild(img);
+    li.appendChild(ptitle);
+
+    li.style.padding = "10px";
+    img.style.width="300px";
+    ptitle.style.color ="white";
+    ptitle.style.textAlign = "center";
     
-            const busca = this.jogos.filter(jogo => jogo.titulo.includes(input));
-            console.log(busca);
+    img.src = imagem;
+    ptitle.textContent = titulo;
+};
 
-            div.textContent = '';
-            for(let jogo of busca) {
-                jogos.mostrar(jogo.titulo, jogo.imagem);
-            }
-        
+const button = document.querySelector(".searchbutton").addEventListener("click", () => {
+
+    montarObjeto();
+
+    const input = document.querySelector("#searchbar").value;
+    const busca = jogos.filter(jogo => jogo.titulo.includes(input));
+    console.log(busca);
+
+    div.textContent = '';
+    for(let jogo of busca) {
+        mostrar(jogo.titulo, jogo.imagem);
     }
-
-}
-
-let jogos = new Jogos();
+});
