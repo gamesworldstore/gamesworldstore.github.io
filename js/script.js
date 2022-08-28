d6f567df0fe113afce8167e47ae4d33c3c6707c1
Richard Wesley <wesleyrichard30@gmail.com>
2022-08-28 12:20:24 -0300

    adição da função armazenamento localStorage

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 
-/*Usuário e senha*/
+/* PARTE DAS FUNÇÕES RESPONSÁVEIS PELO LOGIN */
+
+//Verificação se o usuário já foi inserido ou não, caso não, executa a função logar
 
 let user = false;
 
+const clickUser = document.querySelector('#userid');
+let h3 = document.createElement('h3');
+
 const analise = () => {
-    if(user){}
+    if(localStorage.userSave){
+        user = JSON.parse(localStorage.getItem('userSave'));
+    }
+    if(user){
+        clickUser.appendChild(h3);
+        h3.textContent = '';
+        h3.textContent = user;
+    }
     else{
         logar();
     }
 }
 
-const clickUser = document.querySelector('#userid');
+// Função logar cria um layout de login
 
 const logar = () => {
     caring.style.position = 'static';
+
     itemsCarrinho();
 
     const loginDiv = document.querySelector('.login');
@@ -51,7 +64,9 @@ const logar = () => {
 
     loginButton.setAttribute("onclick", "verificar()");
 
-}
+};
+
+//Função que concatena com a função login, se o usuário não inserir os dados parar logar, vai receber os alertas
 
 const verificar = () => {
     const inputUser = document.querySelector('#inputUser').value;
@@ -70,8 +85,9 @@ const verificar = () => {
     else{
         entrar();
     }
-    
-}
+};
+
+// Função que cria a segunda tela pós login, dando as boas vindas ao usuário no site
 
 const entrar = () => {
     const login = document.querySelector('.login');
@@ -88,6 +104,7 @@ const entrar = () => {
     sair.style.marginTop = '10px';
 
     sair.textContent = 'Continuar';
+
     sair.addEventListener('click', () =>{
 
         let h3 = document.createElement('h3');
@@ -97,9 +114,16 @@ const entrar = () => {
 
         caring.style.position = 'fixed';
         document.querySelector('body').removeChild(login);
+        itemsCarrinho();
     });
 
+    localStorage.userSave = JSON.stringify(user);
 };
+
+
+/* PARTE RESPONSÁVEL PELA BUSCA POR JOGOS E CRIAÇÃO DO ARRAY DE JOGOS */
+
+// Função da barra de pesquisa, faz a busca pelos jogos cadastrados no site clicando enter ou clicando na lupa
 
 const div = document.querySelector(".games");
 div.style.minWidth = "100vmin";
@@ -108,7 +132,43 @@ div.style.maxWidth = "50vmax";
 const jogos = [];
 let id = 1;
 
-/* Busca por Jogo */
+document.querySelector(".searchbutton").addEventListener('click', () => {
+    ocultar(table);
+    first_click = true;
+    criar();
+});
+
+document.querySelector("#searchbar").addEventListener('keypress', (e) =>{
+
+    if(e.key == 'Enter'){
+        ocultar(table);
+        first_click = true;
+        criar();
+    }
+});
+
+// Função que monta faz a busca pelo nome do jogo dentro do array de jogos cadastrados no site utilizando filter e includes;
+
+let controle = true;
+
+const criar = () => {
+    if(controle){
+        montarObjeto();
+    };
+
+    const input = document.querySelector("#searchbar").value;
+    const busca = jogos.filter(jogo => jogo.titulo.includes(input));
+
+    const h1 = document.querySelector("#h1title");
+    h1.textContent = 'Pesquisa';
+    div.textContent = '';
+
+    for(let jogo of busca) {
+        mostrar(jogo.titulo, jogo.imagem, jogo.preco, jogo.id);
+    } 
+};
+
+// Função que monta o objeto array de jogos, utilizando na filtragem acima
 
 const montarObjeto = () => {
     const jogo = document.querySelectorAll("h4.jogo");
@@ -133,6 +193,8 @@ const montarObjeto = () => {
     controle = false;
 };
 
+// Função que exibe na tela o novo objeto criado a partir do array de jogos e filtrado na busca por nome
+
 const mostrar = (titulo, imagem, preco, id) => {
     const li =document.createElement('li');
     const ul = document.createElement('ul').appendChild(li);
@@ -156,35 +218,8 @@ const mostrar = (titulo, imagem, preco, id) => {
     ptitle.textContent = titulo;
     button.textContent = 'R$: ' + preco;
 };
-let controle = true;
 
-const criar = () => {
-    if(controle){
-        montarObjeto();
-    };
-
-    const input = document.querySelector("#searchbar").value;
-    const busca = jogos.filter(jogo => jogo.titulo.includes(input));
-    const h1 = document.querySelector("#h1title");
-    h1.textContent = 'Pesquisa';
-    div.textContent = '';
-    for(let jogo of busca) {
-        mostrar(jogo.titulo, jogo.imagem, jogo.preco, jogo.id);
-    } 
-};
-
-document.querySelector(".searchbutton").addEventListener('click', () => {
-    criar();
-});
-
-document.querySelector("#searchbar").addEventListener('keypress', (e) =>{
-
-    if(e.key == 'Enter'){
-        criar();
-    }
-});
-
-/* Carrinho de Compras */
+/* PARTE RESPONSÁVEL PELA CRIAÇÃO DO CARRINHO DE COMPRAS */
 
 const p = document.createElement('p');
 const caring = document.querySelector("#caring");
@@ -193,27 +228,105 @@ let tbody = document.querySelector("#tbody");
 let thead = document.querySelector("#thead");
 let total = document.querySelector("#total");
 const button = document.createElement('button');
+
 caring.appendChild(p);
 
 caring.style.position = 'fixed';
 
 let n = 0;
-const carrinho = [];
+let carrinho = [];
 
-const somarPrecos = () => {
+//Evento executado no load com o número de itens do carrinho
 
-    let total = carrinho.reduce((valPrev, elemento) => valPrev + parseFloat(elemento.preco), 0);
+window.addEventListener('load', () => {
+    if(localStorage.nSave){
+        n = JSON.parse(localStorage.getItem('nSave'));
+    }
 
-    return total;
-}
+    p.textContent = n;
+    p.style.fontSize = '100%'; 
+    p.style.fontWeight = 'bold';
+    p.style.marginTop = '-30px';
+    p.style.marginLeft = '17px';
+    p.style.textAlign = 'center';
+    p.style.position= "fixed";
+    p.style.color = 'gray';
+
+})
+
+// Função para ocultar a exibição do carrinho ao clicar nele
+
+const ocultar = (table) => {
+    tbody.textContent = '';
+    thead.textContent = '';
+    button.style.display = 'none';
+    table.style.display = 'none';
+    table.border = "0";
+};
+
+// Evento para ver o clique do carrinho, para abrir ou fechar o carrinho
+
+let first_click = true;
+
+caring.addEventListener('click', () => {
+    if(first_click){
+        listar(table);
+        first_click = false;
+    }
+    else{
+        ocultar(table);
+        first_click = true;
+    }
+});
+
+// Função para ocultar a janela o carrinho de compras, utilizada sempre ao fazer uma atualização de adição de novo jogo ao carrinho,
+// ou quando clica no carrinho para fechar, e não ficar exibindo na tela
+
+const itemsCarrinho = () => {
+    p.textContent = n;
+
+    ocultar(table);
+    first_click = true;
+};
+
+// Função responsável pela criação dos objetos que serão inseridos ao array carrinho
+
+const montarCarrinho = (titulo, imagem, preco) => {
+    if(localStorage.carrinhoSave){
+        carrinho = JSON.parse(localStorage.getItem('carrinhoSave'));
+    }
+    if(localStorage.nSave){
+        n = JSON.parse(localStorage.getItem('nSave'));
+    }
+    p.textContent = '';
+
+    let object = {};
+    object.id = n;
+    object.qnt = 1;
+    object.titulo = titulo;
+    object.imagem = imagem;
+    object.preco = parseFloat(preco);
+    
+    carrinho.push(object);
+
+    n++;
+
+    itemsCarrinho();
+    localStorage.nSave = JSON.stringify(n);
+    localStorage.carrinhoSave = JSON.stringify(carrinho);
+};
+
+// Função de Listagem do Carrinho de Compras
 
 const listar = () => {
+    if(localStorage.carrinhoSave){
+        carrinho = JSON.parse(localStorage.getItem('carrinhoSave'));
+    }
 
     table.style.boxShadow = "0 0 15px rgb(125, 125, 125)";
     table.style.backgroundColor = "rgb(36, 11, 98)";
     table.border = "1";
     table.style.display = 'inline';
-    
 
     tbody.textContent = '';
     thead.textContent = '';
@@ -225,11 +338,13 @@ const listar = () => {
     let titulo = th.insertCell();
     let preco = th.insertCell();
     let acoes = th.insertCell();
+
     if(carrinho.length == 0){
+
         th.textContent = 'Carrinho Vazio';
     }
     else{
-        
+
         table.appendChild(button);
         button.style.display = 'inline';
         
@@ -239,6 +354,7 @@ const listar = () => {
         acoes.textContent = 'Ações';
 
         for(let i = 0; i < carrinho.length; i++){
+
             const ex = document.createElement('button');
             const mais = document.createElement('button');
             const menos = document.createElement('button');
@@ -250,7 +366,7 @@ const listar = () => {
             let preco = tr.insertCell();
 
             let excluir = tr.insertCell();
-            
+        
             excluir.classList.add('button_align');
 
             excluir.appendChild(mais);
@@ -284,7 +400,29 @@ const listar = () => {
         button.textContent = 'Comprar';
     }
 };
+
+// Função da Soma dos preços do total de itens dentro do carrinho de compras
+
+const somarPrecos = () => {
+    if(localStorage.carrinhoSave){
+        carrinho = JSON.parse(localStorage.getItem('carrinhoSave'));
+    }
+
+    let total = carrinho.reduce((valPrev, elemento) => valPrev + parseFloat(elemento.preco), 0);
+
+    return total;
+};
+
+// Função aumentar a quantidade de um jogo dentro do carrinho
+
 const addJogo = (id) => {
+    if(localStorage.carrinhoSave){
+        carrinho = JSON.parse(localStorage.getItem('carrinhoSave'));
+    }
+    if(localStorage.nSave){
+        n = JSON.parse(localStorage.getItem('nSave'));
+    }
+
     for(let i = 0; i < carrinho.length; i++){
         if(carrinho[i].id == id) {
             let qnt = carrinho[i].qnt;
@@ -298,13 +436,27 @@ const addJogo = (id) => {
             carrinho[i].preco = preco;
 
             n++;
-            
+
+            localStorage.nSave = JSON.stringify(n);
+            localStorage.carrinhoSave = JSON.stringify(carrinho);
+
             itemsCarrinho();
             listar();
         }
     }
-}
+    
+};
+
+// Função para reduzir a quantidade de um jogo dentro do carrinho, se qnt = 0, o jogo é deletado
+
 const tiraJogo = (id) => {
+    if(localStorage.carrinhoSave){
+        carrinho = JSON.parse(localStorage.getItem('carrinhoSave'));
+    }
+    if(localStorage.nSave){
+        n = JSON.parse(localStorage.getItem('nSave'));
+    }
+
     for(let i = 0; i < carrinho.length; i++){
         if(carrinho[i].id == id) {
             let qnt = carrinho[i].qnt;
@@ -322,14 +474,28 @@ const tiraJogo = (id) => {
                 carrinho[i].preco = preco;
                 n--;
 
+                localStorage.nSave = JSON.stringify(n);
+                localStorage.carrinhoSave = JSON.stringify(carrinho);
+
                 itemsCarrinho();
                 listar();
             }  
         }
     }
+    
 }
 
+// Função que remove o jogo do carrinho
+
 const removerJogo = (id) => {
+    if(localStorage.carrinhoSave){
+        carrinho = JSON.parse(localStorage.getItem('carrinhoSave'));
+    }
+
+    if(localStorage.nSave){
+        n = JSON.parse(localStorage.getItem('nSave'));
+    }
+
     for(let i = 0; i < carrinho.length; i++){
         if(carrinho[i].id == id) {
             let qnt = carrinho[i].qnt;
@@ -339,42 +505,32 @@ const removerJogo = (id) => {
 
             n = n-qnt;
 
+            localStorage.nSave = JSON.stringify(n);
+            localStorage.carrinhoSave = JSON.stringify(carrinho);
+
             itemsCarrinho();
             listar();
         }
     }
 }
 
-const itemsCarrinho = () => {
-    p.textContent = n;
-    ocultar(table);
-    first_click = true;
-}
 
-const montarCarrinho = (titulo, imagem, preco) => {
-    p.textContent = '';
 
-    let object = {};
-    object.id = n;
-    object.qnt = 1;
-    object.titulo = titulo;
-    object.imagem = imagem;
-    object.preco = parseFloat(preco);
-    
-    carrinho.push(object);
+/* PARTE RESPONSÁVEL PELO CLIQUE NOS BOTÕES DE COMPRA DOS JOGOS E ENVIÁ-LOS AO CARRINHO DE COMPRAS */
 
-    n++;
+// Criação de um array com os preços dos jogos assim que o site abrir
 
-    p.style.fontSize = '100%'; 
-    p.style.fontWeight = 'bold';
-    p.style.marginTop = '-30px';
-    p.style.marginLeft = '17px';
-    p.style.textAlign = 'center';
-    p.style.position= "fixed";
-    p.style.color = 'gray';
+const precos = [];
 
-    itemsCarrinho();
-};
+window.addEventListener('load', () => {
+    const buy = document.querySelectorAll("p.preco");
+
+    for(let i = 0; i<buy.length; i++){
+        precos.push(buy[i].textContent);
+    }
+});
+
+// Função que verifica se o jogo já foi adicionado ao carrinho através do título, para não ficar repetido
 
 const verifica = (titulo) => {
     for(let i = 0; i < carrinho.length; i++){
@@ -385,14 +541,9 @@ const verifica = (titulo) => {
     return true;
 
 };
-const precos = [];
 
-window.addEventListener('load', () => {
-    const buy = document.querySelectorAll("p.preco");
-    for(let i = 0; i<buy.length; i++){
-        precos.push(buy[i].textContent);
-    }
-});
+// Função responsável por adicionar jogos ao carrinho quando clicar em qualquer botão de comprar jogo nos jogos disponíveis
+// Essa mesma função é implementada tanto no home quanto quando você pesquisa por um jogo
 
 const comparar = (n) => {
     if(controle){
@@ -408,36 +559,24 @@ const comparar = (n) => {
     }
 };
 
-/* Exibir e ocultar carrinho */
 
-let first_click = true;
-
-const ocultar = (table) => {
-    tbody.textContent = '';
-    thead.textContent = '';
-    button.style.display = 'none';
-    table.style.display = 'none';
-    table.border = "0";
-}
-
-caring.addEventListener('click', () => {
-    if(first_click){
-        listar(table);
-        first_click = false;
-    }
-    else{
-        ocultar(table);
-        first_click = true;
-    }
-});
-
-/* Fazer a Compra */
+/* PARTE RESPONSÁVEL PELO CLIQUE NO BOTÃO COMPRAR, EFETUAR A COMPRA DOS JOGOS NO CARRINHO */
 
 const ok = document.createElement('button');
 const done = document.querySelector('.done');
 const doneId = document.querySelector('#done');
 
+// Botão comprar, ao clicar nele, vai verificar se o usuário já foi logado, caso contrário vai pedir para efetuar o login
+// Logo depois vai limpar o carrinho de compras
+// E logo depois vai inicializar uma tela que irá dizer que a compra foi efetuada
+
 button.addEventListener('click', () => {
+    if(localStorage.userSave){
+        user = JSON.parse(localStorage.getItem('userSave'));
+    }
+    if(localStorage.nSave){
+        n = JSON.parse(localStorage.getItem('nSave'));
+    }
 
     if(user){
         while(carrinho.length){
@@ -462,13 +601,16 @@ button.addEventListener('click', () => {
         h1.textContent = 'Compra Concluída!';
         ok.textContent = 'OK';
         caring.style.position = 'static';
+
+        localStorage.nSave = JSON.stringify(n);
+        localStorage.carrinhoSave = JSON.stringify(carrinho);
     }
     else{
         logar();
     }
-
-    
 });
+
+// Botão e evento exibido da compra efetuada, ao clicar em ok irá sumir a tela de compra efetuada
 
 ok.addEventListener('click', () =>{
     doneId.style.width = '0%';
